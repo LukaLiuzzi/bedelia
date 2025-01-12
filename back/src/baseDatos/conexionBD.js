3dda42c00378880e9c244387cc376d9ce17058bd
lukaliuzzi <lukaliuzzidev@gmail.com>
2025-01-11 23:46:57 -0300

    Add Docker support and environment configuration; update API URL handling and bcrypt dependency

--- back/src/baseDatos/conexionBD.js
+++ back/src/baseDatos/conexionBD.js
@@ -1,12 +1,11 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise")
 
 // pool de conexiones a la base de datos
 const conexion = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-});
-
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+})
 
 module.exports = conexion
--- back/src/controladores/registro.js
+++ back/src/controladores/registro.js
@@ -1,42 +1,49 @@
-const { Router } = require('express');
-const router = Router();
-const registroDB = require('../baseDatos/registroDB'); 
+const { Router } = require("express")
+const router = Router()
+const registroDB = require("../baseDatos/registroDB")
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs")
 
-const crearUsuario = async (req, res) => { // Definir crearUsuario antes de usarlo
-    const { correoElectronico, nombre, apellido, clave } = req.body;
-    console.log('Datos recibidos en req.body:', req.body);
+const crearUsuario = async (req, res) => {
+  // Definir crearUsuario antes de usarlo
+  const { correoElectronico, nombre, apellido, clave } = req.body
+  console.log("Datos recibidos en req.body:", req.body)
 
-    if (!correoElectronico || !nombre || !apellido || !clave) {
-        res.status(400).json({ estado: 'FALLA', msj: 'Faltan datos obligatorios' });
-    } else {
-        // Hashear la contraseña antes de guardarla en la base de datos
-        bcrypt.hash(clave, 10, async (err, hashedClave) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({ estado: 'FALLA', msj: 'Error al hashear la contraseña' });
-            } else {
-                const usuario = {
-                    correoElectronico: correoElectronico,
-                    nombre: nombre,
-                    apellido: apellido,
-                    clave: hashedClave // Usamos la contraseña hasheada
-                };
+  if (!correoElectronico || !nombre || !apellido || !clave) {
+    res.status(400).json({ estado: "FALLA", msj: "Faltan datos obligatorios" })
+  } else {
+    // Hashear la contraseña antes de guardarla en la base de datos
+    bcrypt.hash(clave, 10, async (err, hashedClave) => {
+      if (err) {
+        console.error(err)
+        res
+          .status(500)
+          .json({ estado: "FALLA", msj: "Error al hashear la contraseña" })
+      } else {
+        const usuario = {
+          correoElectronico: correoElectronico,
+          nombre: nombre,
+          apellido: apellido,
+          clave: hashedClave, // Usamos la contraseña hasheada
+        }
 
-                try {
-                    const usuarioNuevo = await registroDB.crearUsuario(usuario);
+        try {
+          const usuarioNuevo = await registroDB.crearUsuario(usuario)
 
-                    res.status(201).json({ estado: 'ok', msj: 'Usuario creado', dato: usuarioNuevo });
-                } catch (error) {
-                    console.error(error);
-                    res.status(500).json({ estado: 'FALLA', msj: 'Error en la creación del usuario' });
-                }
-            }
-        });
-    }
-};
+          res
+            .status(201)
+            .json({ estado: "ok", msj: "Usuario creado", dato: usuarioNuevo })
+        } catch (error) {
+          console.error(error)
+          res
+            .status(500)
+            .json({ estado: "FALLA", msj: "Error en la creación del usuario" })
+        }
+      }
+    })
+  }
+}
 
 module.exports = {
-    crearUsuario
-};
+  crearUsuario,
+}
